1356835a27d8edc3f43ea42eb54e95ac68aad7d3
Ly Sanim <113698362+LySanim@users.noreply.github.com>
2023-09-25 23:42:30 +0700

    Update cats.js

--- src/pages/cats.js
+++ src/pages/cats.js
@@ -4,6 +4,7 @@ import CatCard from "../components/CatCard";
 const Cats =()=>{
     return(
         <div>
+            <h1>Display all card</h1>
             <Container className="d-flex justify-content-center">
             <CatCard/>
             </Container>
